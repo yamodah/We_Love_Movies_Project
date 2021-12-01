1de49a6e51775504e68bd9bd9e04e645140d1386
yamodah <yassinedahlek@gmail.com>
2021-12-01 12:09:14 -0500

    formatting

--- src/db/migrations/20211201112537_createCriticsTable.js
+++ src/db/migrations/20211201112537_createCriticsTable.js
@@ -1,14 +1,13 @@
-
-exports.up = function(knex) {
-  return knex.schema.createTable('critics',(table)=>{
-      table.increments('critic_id').primary()
-      table.string('preferred_name')
-      table.string('surname')
-      table.string('oragnization_name')
-      table.timestamps([useTimestamps], [defaultToNow])
-  })
+exports.up = function (knex) {
+  return knex.schema.createTable("critics", (table) => {
+    table.increments("critic_id").primary();
+    table.string("preferred_name");
+    table.string("surname");
+    table.string("oragnization_name");
+    table.timestamps([useTimestamps], [defaultToNow]);
+  });
 };
 
-exports.down = function(knex) {
-  return knex.schema.dropTable('critics')
+exports.down = function (knex) {
+  return knex.schema.dropTable("critics");
 };
