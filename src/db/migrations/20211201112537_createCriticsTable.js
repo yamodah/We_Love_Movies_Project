00cd239e838a9c23ad705cb912045037d4a93c8a
yamodah <yassinedahlek@gmail.com>
2021-12-01 12:36:26 -0500

    added reviews logic and changed timestamps

--- src/db/migrations/20211201112537_createCriticsTable.js
+++ src/db/migrations/20211201112537_createCriticsTable.js
@@ -4,7 +4,7 @@ exports.up = function (knex) {
     table.string("preferred_name");
     table.string("surname");
     table.string("oragnization_name");
-    table.timestamps([useTimestamps], [defaultToNow]);
+    table.timestamps(true,true);
   });
 };
 
