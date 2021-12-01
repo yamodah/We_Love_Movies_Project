4a98baf5d776617546a0f00f536f74e2b5ac80ef
yamodah <yassinedahlek@gmail.com>
2021-12-01 12:51:50 -0500

    movie_theaters lgic added

--- src/db/migrations/20211201113135_createMovieTheatersTable.js
+++ src/db/migrations/20211201113135_createMovieTheatersTable.js
@@ -1,8 +1,22 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('movie_theaters',(table)=>{
+      table.integer('movie_id').unsigned().notNullable()
+      table
+        .foreign('movie_id')
+        .references('movie_id')
+        .inTable('movies')
+        .onDelete('cascade')
+      table.integer('theater_id')
+      table
+        .foreign('theater_id')
+        .references('theater_id')
+        .inTable('theater')
+        .onDelete('cascade')
+      table.boolean('is_showing')
+  })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTable('movie_theaters')
 };
