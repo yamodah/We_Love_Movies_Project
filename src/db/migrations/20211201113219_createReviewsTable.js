45cd783e7ef1e5156a21565cd82b411e2bf15ce2
yamodah <yassinedahlek@gmail.com>
2021-12-01 13:07:09 -0500

    fixed spelling issue for intger to integer

--- src/db/migrations/20211201113219_createReviewsTable.js
+++ src/db/migrations/20211201113219_createReviewsTable.js
@@ -3,8 +3,8 @@ exports.up = function(knex) {
   return knex.schema.createTable('reviews',(table)=>{
       table.increments('review_id').primary()
       table.text('content')
-      table.intger('score')
-      table.intger('movie_id').unsigned().notNullable()
+      table.integer('score')
+      table.integer('movie_id').unsigned().notNullable()
       table
         .foreign('movie_id')
         .references('movie_id')
