021f7e3a728523ff01c3bac8d96d72c91109ec75
yamodah <yassinedahlek@gmail.com>
2021-12-01 16:11:31 -0500

    built out some of movie.router and some of the app.js

--- src/movies/movies.router.js
+++ src/movies/movies.router.js
@@ -1,7 +1,12 @@
 const controller = require("./movies.controller")
 const router = require("express").Router()
-
+const  methodNotAllowed =  require("../errors/methodNotAllowed")
 router
     .route("/:movieId")
     .get(controller.read)
-    
+    .all(methodNotAllowed);
+
+router
+    .route("/")
+    .get(controller.list)
+    .all(methodNotAllowed)
